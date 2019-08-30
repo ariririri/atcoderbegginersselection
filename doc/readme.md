fdbc0ee7d5f87e0b4da15388a7e04b1e3171471d
ariririri <fatal.t.h.f.flydream.bamboo@gmail.com>
2019-08-30 15:04:36 +0900

    add all abc

--- doc/readme.md
+++ doc/readme.md
@@ -1,12 +1,16 @@
 # AtCoder Rust
 
+今回はrustの理解も兼ねて
 [AtCoder Beginners Selection](https://atcoder.jp/contests/abs)を解いてみます.
+
+自分が水色なので、そのレベルの知識があれば、解くのは難しくないので、
+問題を解くことではなく、rustの文法に慣れることを目指して解説していきます.
 
 - 文字列の扱い
 - 標準入力の対応
 - 実際に解いてみる.
-- 1~10
-- マクロを使い入出力を簡単にする.
+- マクロを使い標準入力の改善
+- 他の人でいい解答がないか調査.
 
 ## 文字列の扱い
 競技プログラミングでは入力を受けて処理をするので、まず入力の文字列を処理する方法を考えます.
@@ -79,7 +83,6 @@ fn longer(x: &str, y: &str) -> String {
 ### 標準入力
 
 rustの標準入力はread_lineが使われます.
-[ドキュメント](https://doc.rust-lang.org/std/io/struct.Stdin.html)を見ればわかるように,引数にした変数に入力をわたし、復帰値のResult型でうまくいったかどうかを表します.
 引数は必ず文字列型になるため、AtCoder用には条件に応じて型を変換する必要があります.
 
 ```rust
@@ -90,12 +93,180 @@ AtCoderでそのまま読むには[これ](https://qiita.com/penguinshunya/items
 
 前置きが相当長くなったので、この辺から真面目にAtCoderときましょう.
 
+今回は上のサイトにある`read`のコードを使いました.また,全てを使うわけではないので、何も考えずにコンパイルするとwarningが出ます.
+なので、コードの先頭に`#![allow(dead_code)]`と書くことを推奨しておきます.
+
+
+### ABC081B
+
+問題はループの中で再帰的に2で割っていけば,求まると思います.
+rustの場合,条件を満たすまでずっとループするのは
+`loop`でかけます.脱出の時はbreakですが、今回のようにforでネストしてしまう場合はスコープを指定する必要があります.
+
+```rust
+fn abc081() {
+    let n: usize = read();
+    let mut A: Vec<i32> = read_vec();
+    let mut ret = 0;
+    'outer: loop {
+        for i in 0..n {
+            if A[i] % 2 == 1 {
+                break 'outer;
+            } else {
+                A[i] /= 2;
+            }
+        }
+        ret += 1;
+    };
+    println!("{}", ret);
+}
+```
+
+### ABC087B
+1250通りしかないので、愚直に全探索します.
+```rust
+fn abc087b() {
+    let a: i32 = read();
+    let b: i32 = read();
+    let c: i32 = read();
+    let x: i32 = read();
+    let mut num = 0;
+    for _a in 0..(a+1) {
+        for _b in 0..(b+1) {
+            for _c in 0..(c+1) {
+                if 500 *_a + 100 * _b + 50 * _c  == x {
+                    num += 1;
+                }
+            }
+        }
+    }
+    println!("{}", num);
+}
+```
+
+### ABC083B
+これも10000個しかないので、全探索します.
+桁数の和は数値を文字列に変形して,一文字ずつ分割した後足しました.
+rustの場合はchar型の列にして、それを文字コードをみながら整数に置き直しています.
+```rust
+fn abc083b(){
+    let input: Vec<i32> = read_vec();
+    let (n, a, b) = (input[0], input[1], input[2]);
+    let mut num = 0;
+    for _n in 1..(n+1) {
+        let _n_sum: i32 = _n.to_string().chars().map(|c| c as i32 - 48).fold(0, |sum, c| sum + c);
+        if _n_sum >= a  && _n_sum <= b {
+            num += _n;
+        }
+    }
+    println!("{}", num);
+}
+```
+
+### ABC088B
+全体をソートして差分をひきます.
+```rust
+fn abc088b() {
+    let n: usize = read();
+    let mut a: Vec<i32> = read_vec();
+    let mut num = 0;
+    a.sort();
+    for _n in 0..n {
+        if _n % 2 == 0 {
+           num += a[n-1-_n];
+        } else {
+            num -= a[n-1-_n];
+        }
+    }
+    println!("{}", num);
+}
+```
+### ABC085B
+重複排除して、サイズを数えればよいですね。
+重複排除するときはsetにするのが自然かと思いますが、
+rustの場合はHashSetクラスが存在するので、それで計算します.
+
+```rust
+fn abc085b(){
+    let n: u32 = read();
+    let d: Vec<Vec<u32>> = read_vec2(n);
+    let uniq_d: HashSet<u32> = d.into_iter().map(|x| x[0]).collect();
+    println!("{}", uniq_d.len());
+}
+```
+
+### ABC085C
+条件を考えると
+1000円札をz枚使ったとき,
+10000円札をx枚、5000円札をy枚使う解答があるかどうかは
+- Y - 1000 z が5000で割り切れ
+以下の方程式について、x,yを解くと0以上の整数解(今回は整数には必ずなので、)になればよいです。
+
+- 2x + y = (Y - 1000 z) / 5000
+- x + y =  N - z
+
+それは x = (Y -1000 z) / 5000  - N + z >= 0かつ
+y = N - z - x >= 0で判定できます.
+
 
 
 実際、↓を読めばよくわかると思います.
 - https://qiita.com/tubo28/items/e6076e9040da57368845
 - https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
 
+### ABC049C
+貪欲に解けばいいので、
 
-## わからないもの
--
+1文字目がe 
+  - eraser or erase
+  - 文字の長さが5かつeraseならYes
+  - 文字の長さが6かつeraserならYes
+  - 文字列の長さが6以下で上記以外の場合No
+  - 6文字目がrならeraserを削除
+  - 6文字目がe or dなら eraseを削除
+  - それ以外No
+- dreamの場合も同様に分けていけばできます.
+ただし、dreamera,dreamerd等で区別する必要はあります.
+
+
+### ABC086 C
+移動時間が最短移動時間＋2n(n >= 0)であればよいので、
+それに合わせて実装します.
+
+```rust
+fn abc086c(){
+    let n: u32 = read();
+    let input: Vec<Vec<i32>> = read_vec2(n);
+    let mut old_x = 0;
+    let mut old_y = 0;
+    let mut old_t = 0;
+    let mut flag = true;
+    for inp_i in input {
+        let (t, x, y) = (inp_i[0], inp_i[1], inp_i[2]);
+        let diff = t - old_t - (x - old_x).abs() - (y - old_y).abs();
+        if diff < 0 || diff % 2 == 1 {
+            println!("No");
+            flag = false;
+            break;
+        } else{
+            old_t = t;
+            old_x = x;
+            old_y = y;
+        }
+    }
+    if flag {
+        println!("Yes");
+    }
+}
+```
+
+### よりよいrustにするために
+
+
+初めてやって思ったこと
+
+- 文字列の扱いが難しい.
+- Result型にするのは面白い.
+- 配列のindexがuintでないとエラーになるのはつらい.
+- numpyはすごいなという気持ちに...
+  - (np.where含め集合的な処理ができたが、rustのndarrayではできず.)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,245 @@
+#![allow(dead_code)]
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mode = "abc081";
+    if mode == "prcatice" {
+        practice();
+    } else if mode == "086" {
+        abc086a();
+    } else if mode == "abc081" {
+        abc081a();
+    } else if mode == "abc081b" {
+        abc081b();
+    } else if mode == "abc087b" {
+        abc087b();
+    } else if mode == "abc083b" {
+        abc083b();
+    } else if mode == "abc088b" {
+        abc088b();
+    } else if mode == "abc085b" {
+        abc085b();
+    } else if mode == "abc085c" {
+        abc085c();
+    } else if mode == "abc049c" {
+        abc049c();
+    } else if mode == "abc086c" {
+        abc086c();
+    }
+}
+
+fn abc086c(){
+    let n: u32 = read();
+    let input: Vec<Vec<i32>> = read_vec2(n);
+    let mut old_x = 0;
+    let mut old_y = 0;
+    let mut old_t = 0;
+    let mut flag = true;
+    for inp_i in input {
+        let (t, x, y) = (inp_i[0], inp_i[1], inp_i[2]);
+        let diff = t - old_t - (x - old_x).abs() - (y - old_y).abs();
+        if diff < 0 || diff % 2 == 1 {
+            println!("No");
+            flag = false;
+            break;
+        } else{
+            old_t = t;
+            old_x = x;
+            old_y = y;
+        }
+    }
+    if flag {
+        println!("Yes");
+    }
+}
+
+
+fn abc049c(){
+    let mut s: String = read();
+    loop {
+        let len = s.len();
+        if len < 5 {
+            println!("NO");
+            break
+        }
+        if s == "erase" || s == "eraser" || s == "dream" || s == "dreamer" {
+            println!("YES");
+            break
+        }
+        if s.chars().nth(0).unwrap() == 'e' {
+            if &s[0..6] == "eraser" {
+                s = s[6..len].to_string();
+            } else if &s[0..5] == "erase" {
+                s = s[5..len].to_string();
+            } else {
+                println!("NO");
+                break
+            }
+        } else if s.chars().nth(0).unwrap() == 'd' {
+            if &s[0..7] == "dreamer" {
+                if &s[0..8] == "dreamera" {
+                    s = s[5..len].to_string();
+                } else {
+                    s = s[7..len].to_string();
+                }
+            } else if &s[0..5] == "dream" {
+                s = s[5..len].to_string();
+            } else {
+                println!("NO");
+                break
+            }
+        } else {
+            println!("NO");
+            break
+        }
+    }
+
+}
+
+fn abc085c(){
+    let input: Vec<i32> = read_vec();
+    let (n, Y) = (input[0], input[1]);
+    let mut flag = false;
+    for z in 0..(n+1) {
+        if (Y - 1000 * z) % 5000 != 0 { continue }
+        let div_y = (Y - 1000 * z) / 5000;
+        let x = div_y - n + z;
+        let y = n - z - x;
+        if x < 0 || y < 0 { continue }
+        flag = true;
+        println!("{} {} {}", x, y, z);
+        break
+    }
+    if !flag {
+        println!("{} {} {}", -1, -1, -1);
+    }
+}
+
+fn abc085b(){
+    let n: u32 = read();
+    let d: Vec<Vec<u32>> = read_vec2(n);
+    let uniq_d: HashSet<u32> = d.into_iter().map(|x| x[0]).collect();
+    println!("{}", uniq_d.len());
+}
+
+
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().split_whitespace()
+        .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    let mut v2 = Vec::new();
+    for _ in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).ok();
+        let v = s.trim().split_whitespace()
+            .map(|e| e.parse().ok().unwrap()).collect();
+        v2.push(v);
+    }
+    v2
+}
+
+ 
+fn practice() {
+    let first_int: Vec<i32> = read_vec();
+    let second_int: Vec<i32> = read_vec();
+    let s: String = read();
+    let mut res = first_int[0];
+    res += second_int[0] + second_int[1];
+    println!("{} {}", res, s);
+}
+
+fn abc086a() {
+    let fline: Vec<i32> = read_vec();
+    if (fline[0] * fline[1]) % 2 == 1 {
+        println!("Odd");
+    } else {
+        println!("Even");
+    }
+}
+
+fn abc081a() {
+    let s: String = read();
+    let mut num = 0;
+    for i in 0..3 {
+        if s.chars().nth(i).unwrap() == '1' {
+            num += 1;
+        }
+    }
+    println!("{}", num);
+
+}
+
+fn abc081b() {
+    let n: usize = read();
+    let mut A: Vec<i32> = read_vec();
+    let mut ret = 0;
+    'outer: loop {
+        for i in 0..n {
+            if A[i] % 2 == 1 {
+                break 'outer;
+            } else {
+                A[i] /= 2;
+            }
+        }
+        ret += 1;
+    };
+    println!("{}", ret);
+
+}
+
+fn abc087b() {
+    let a: i32 = read();
+    let b: i32 = read();
+    let c: i32 = read();
+    let x: i32 = read();
+    let mut num = 0;
+    for _a in 0..(a+1) {
+        for _b in 0..(b+1) {
+            for _c in 0..(c+1) {
+                if 500 *_a + 100 * _b + 50 * _c  == x {
+                    num += 1;
+                }
+            }
+        }
+    }
+    println!("{}", num);
+}
+
+fn abc083b(){
+    let input: Vec<i32> = read_vec();
+    let (n, a, b) = (input[0], input[1], input[2]);
+    let mut num = 0;
+    for _n in 1..(n+1) {
+        let _n_sum: i32 = _n.to_string().chars().map(|c| c as i32 - 48).fold(0, |sum, c| sum + c);
+        if _n_sum >= a  && _n_sum <= b {
+            num += _n;
+        }
+    }
+    println!("{}", num);
+}
+
+fn abc088b() {
+    let n: usize = read();
+    let mut a: Vec<i32> = read_vec();
+    let mut num = 0;
+    a.sort();
+    for _n in 0..n {
+        if _n % 2 == 0 {
+           num += a[n-1-_n];
+        } else {
+            num -= a[n-1-_n];
+        }
+    }
+    println!("{}", num);
 }
